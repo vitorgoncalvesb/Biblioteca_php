9239083a030934c407c9f5b3a633cdbd44cda956
Vitor G. <160020798+vitorgoncalvesb@users.noreply.github.com>
2025-08-11 09:04:50 -0300

    Add files via upload

---
+++ biblioteca/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
+        rel="stylesheet" />
+</head>
+
+<body>
+    <main class="p-6 max-w-6xl mx-auto">
+        <h1 class="text-2xl font-bold mb-6">Biblioteca</h1>
+
+        <div class="flex gap-4">
+            <a href="listar.php" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
+                Acervo
+            </a>
+
+            <a href="form_cadastro.php" class="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition">
+                Inserir Livro
+            </a>
+        </div>
+
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</body>
+
+</html>
